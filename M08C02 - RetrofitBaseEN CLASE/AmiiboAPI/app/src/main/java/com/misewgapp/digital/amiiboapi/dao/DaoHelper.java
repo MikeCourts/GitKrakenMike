98a4ea972732cc5ba6b0f79468b0ba9884487509
Mike Courts <courts.mike@gmail.com>
2018-11-05 19:13:03 -0300

    asd

---
+++ M08C02 - RetrofitBaseEN CLASE/AmiiboAPI/app/src/main/java/com/misewgapp/digital/amiiboapi/dao/DaoHelper.java
@@ -0,0 +1,27 @@
+package com.misewgapp.digital.amiiboapi.dao;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public abstract class DaoHelper {
+    protected Retrofit retrofit;
+
+    public DaoHelper(String base_url) {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        Retrofit.Builder builder =
+                new Retrofit.Builder()
+                        .baseUrl(base_url)
+                        .addConverterFactory(
+                                GsonConverterFactory.create()
+                        );
+
+        retrofit =
+                builder
+                        .client(
+                                httpClient.build()
+                        )
+                        .build();
+    }
+}
---
+++ M08C02 - RetrofitBaseEN CLASE/AmiiboAPI/app/src/main/java/com/misewgapp/digital/amiiboapi/util/ResultListener.java
@@ -0,0 +1,5 @@
+package com.misewgapp.digital.amiiboapi.util;
+
+public interface ResultListener<T> {
+    void finish(T Resultado);
+}
