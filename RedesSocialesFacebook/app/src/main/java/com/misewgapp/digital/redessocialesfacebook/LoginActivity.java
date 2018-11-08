39df671fb703d402992f02825c3d937c74633a15
Mike Courts <courts.mike@gmail.com>
2018-11-07 22:04:10 -0300

    asd

---
+++ RedesSocialesFacebook/app/src/main/java/com/misewgapp/digital/redessocialesfacebook/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.misewgapp.digital.redessocialesfacebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
