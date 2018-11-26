127635a2fcf1f8bf8c0bb448b4cfb809dacc29fa
Mike Courts <courts.mike@gmail.com>
2018-11-26 17:19:38 -0300

    asd

--- MikeCourts/app/src/main/java/com/misewgapp/digital/mikecourts/view/LoginActivity.java
+++ MikeCourts/app/src/main/java/com/misewgapp/digital/mikecourts/view/LoginActivity.java
@@ -5,7 +5,9 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,7 +32,10 @@ import com.misewgapp.digital.mikecourts.util.Util;
 
 public class LoginActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
-    private FirebaseAuth mAuth;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private ProgressBar progressBar;
+    private LoginButton loginButton;
 
 
 
@@ -39,68 +44,78 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-        mAuth = FirebaseAuth.getInstance();
-        mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
-            }
-        });
-
-
+        progressBar = findViewById(R.id.progress_bar);
+        loginButton = findViewById(R.id.login_button);
 
         // FACEBOOK/FIREBASE LOGIN
         callbackManager = CallbackManager.Factory.create();
 
-        final LoginButton loginButton = findViewById(R.id.login_button);
+
         loginButton.setReadPermissions("email", "public_profile");
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
-                Toast.makeText(LoginActivity.this, "Exito", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
 
                 // FIREBASE LOGIN
                 handleFacebookAccessToken(loginResult.getAccessToken());
-
-
             }
 
             @Override
             public void onCancel() {
-
+                Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onError(FacebookException error) {
-
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
             }
         });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    goRecyclerActivity();
+                }
+            }
+        };
+
+
+
+
 
 
     }
 
     private void handleFacebookAccessToken(AccessToken token) {
+//        progressBar.setVisibility(View.VISIBLE);
+//        loginButton.setVisibility(View.GONE);
 
         AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        mAuth.signInWithCredential(credential)
+        firebaseAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+                        if (!task.isSuccessful()) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                            }
+//                            progressBar.setVisibility(View.GONE);
+//                            loginButton.setVisibility(View.VISIBLE);
                         }
                         // ...
                     }
                 });
+    }
+
+    private void goRecyclerActivity() {
+        Intent intent = new Intent(this, RecyclerActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
 
@@ -133,12 +148,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+    }
+
 }
 
 
-//    Intent intent = new Intent(LoginActivity.this, RecyclerActivity.class);
-//    Bundle bundle = new Bundle();
-//            bundle.putString(RecyclerActivity.KEY_NAME, user.getDisplayName());
-//                    bundle.putString(RecyclerActivity.KEY_URI, String.valueOf(user.getPhotoUrl()));
-//                    intent.putExtras(bundle);
-//                    startActivity(intent);
--- MikeCourts/app/src/main/java/com/misewgapp/digital/mikecourts/view/RecyclerActivity.java
+++ MikeCourts/app/src/main/java/com/misewgapp/digital/mikecourts/view/RecyclerActivity.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.misewgapp.digital.mikecourts.R;
 
 public class RecyclerActivity extends AppCompatActivity {
     public static final String KEY_NAME = "facebookUserName";
     public static final String KEY_URI = "uri";
-    private TextView textView;
-    private ImageView imageView;
+    private TextView nameTextView;
+    private ImageView photoImageView;
+    private Button logoutButton;
+    private android.support.v7.widget.Toolbar toolbar;
 
 
     @Override
@@ -22,17 +33,76 @@ public class RecyclerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
 
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
+        nameTextView = findViewById(R.id.textViewNombre);
+        photoImageView = findViewById(R.id.imageViewFBPhoto);
+        logoutButton = findViewById(R.id.logoutButton);
 
-        String name = bundle.getString(KEY_NAME);
-        Uri uri = Uri.parse(bundle.getString(KEY_URI));
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Paintings");
 
-        textView.setText(name);
-        Glide.with(this).load(uri).into(imageView);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null){
+            String name = user.getDisplayName();
+            Uri photo = user.getPhotoUrl();
+            String uid = user.getUid();
+
+            nameTextView.setText(name);
+            Glide.with(this).load(photo).into(photoImageView);
+        } else {
+            goLoginScreen();
+        }
+
+
+
+//        textView.setText(name);
+//        Glide.with(this).load(uri).into(imageView);
 
 
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logoutMenu:
+                logout();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // FIREBASE & FACEBOOK METHODS
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void logout(){
+        FirebaseAuth.getInstance().signOut();
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
+    }
+
+
 }
